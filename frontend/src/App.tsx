61a1108ff840847cfe5f1c73f18177572bda113c
Damian Perera <damianperera@outlook.com>
2024-02-24 15:32:09 +0100

    added table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -61,7 +61,7 @@ function App() {
   const DEFAULT_STATION = 'Forstenrieder Allee'
   const TYPE_STATION = 'STATION'
   const [departureStations, setDepartureStation] = useState<DepartureStationProps>()
-  const [departures, setDepartures] = useState<DepartureProps []>([])
+  const [departures, setDepartures] = useState<DepartureProps [][][]>([])
     // const [disruptions, setDisruptions] = useState<DisruptionProps[]>([])
 
   useEffect(() => {
@@ -75,10 +75,11 @@ function App() {
       data = await fetch(`https://www.mvg.de/api/fib/v2/departure?globalId=${pds?.globalId}&limit=10&offsetInMinutes=0`, {
         method: "GET"
       });
-      jsonData = await data.json();
+      const departures: DepartureProps[] = await data.json();
+      const sortedDepartures: DepartureProps[][][] = sortDepartures(departures)
 
       setDepartureStation(pds)
-      setDepartures(jsonData);
+      setDepartures(sortedDepartures);
     }
 
     // const getDisruptions = async () => {
@@ -114,15 +115,47 @@ function App() {
 
     let remainingTimeString = '';
     if (hours > 0) {
-      remainingTimeString += `in ${hours} ${hours === 1 ? 'hr' : 'hrs'}`;
+      remainingTimeString += `${hours} ${hours === 1 ? 'hr' : 'hrs'}`;
       if (minutes > 0) {
-        remainingTimeString += `in ${minutes} ${minutes === 1 ? 'min' : 'mins'}`;
+        remainingTimeString += `${minutes} ${minutes === 1 ? 'min' : 'mins'}`;
       }
     } else {
-      remainingTimeString += `in ${minutes} ${minutes === 1 ? 'min' : 'mins'}`;
+      remainingTimeString += `${minutes} ${minutes === 1 ? 'min' : 'mins'}`;
     }
 
     return (<span>{remainingTimeString}</span>);
+  };
+
+  const sortDepartures = (departures: DepartureProps[]): DepartureProps[][][] => {
+    // Organize departures into groups based on transportType
+    const groups: { [key: string]: DepartureProps[] } = {};
+    departures.forEach(departure => {
+      const { transportType } = departure;
+      if (!(transportType in groups)) {
+        groups[transportType] = [];
+      }
+      groups[transportType].push(departure);
+    });
+  
+    // Sort groups based on the presence of UBAHN transportType
+    const sortedGroups = Object.entries(groups).sort(([typeA], [typeB]) => {
+      if (typeA === 'UBAHN') return -1;
+      if (typeB === 'UBAHN') return 1;
+      return 0;
+    });
+  
+    // Sort nested arrays within each group by label and objects by realtimeDepartureTime
+    sortedGroups.forEach(([_, departures]) => {
+      departures.sort((a, b) => {
+        if (a.label < b.label) return -1;
+        if (a.label > b.label) return 1;
+        return 0;
+      });
+  
+      departures.sort((a, b) => a.realtimeDepartureTime - b.realtimeDepartureTime);
+    });
+  
+    return sortedGroups.map(([_, departures]) => [departures]);
   };
 
   return (
@@ -131,11 +164,25 @@ function App() {
         <div>
           <h3>{departureStations?.name}</h3>
         </div>
-        {departures.map((departure, idx) => {
-          return (<div key={idx}>
-            <div >{departure.label} to <strong>{departure.destination}</strong> {calculateRemainingTime(departure.realtimeDepartureTime)}</div>
-          </div>)
-        })}
+        <div>
+          <table>
+            <tbody>
+              {departures.map((group) => (
+                group.map((nestedArray, nIdx) => (
+                  <React.Fragment key={nIdx}>
+                    {nestedArray.map((departure, dIdx) => (
+                      <tr key={dIdx}>
+                        <td>{departure.label}</td>
+                        <td>{departure.destination}</td>
+                        <td>{calculateRemainingTime(departure.realtimeDepartureTime)}</td>
+                      </tr>
+                    ))}
+                  </React.Fragment>
+                ))
+            ))}
+      </tbody>
+          </table>
+        </div>
         {/* {disruptions.map((disruption, idx) => {
           return (
             <div key={idx}>
