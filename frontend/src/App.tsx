360ca789accc0afd4172acc0f6cf1064d6835e69
Damian Perera <damianperera@outlook.com>
2024-02-24 14:13:24 +0100

    added departures, station fetch and disruptions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,10 +22,63 @@ type DisruptionProps = {
   lines: DisruptionLineProps[];
 }
 
+type DepartureStationProps = {
+  type: string;
+  latitude: string;
+  longitude: string;
+  place: string;
+  name: string;
+  globalId: string;
+  divaId: string;
+  transportTypes: [];
+  surroundingPlanLink: string;
+  aliases: string;
+  tariffZones: string;
+}
+
+type DepartureProps = {
+  plannedDepartureTime: string;
+  realtime: boolean;
+  delayInMinutes: number;
+  realtimeDepartureTime: number;
+  transportType: string;
+  label: string;
+  divaId: string;
+  network: string;
+  trainType: string;
+  destination: string;
+  cancelled: boolean;
+  sev: boolean;
+  platform: number;
+  platformChanged: boolean;
+  messages: [];
+  bannerHash: string;
+  occupancy: string;
+  stopPointGlobalId: string;
+}
+
 function App() {
+  const [departureStations, setDepartureStation] = useState<DepartureStationProps>()
   const [disruptions, setDisruptions] = useState<DisruptionProps[]>([])
+  const [departures, setDepartures] = useState<DepartureProps []>([])
 
   useEffect(() => {
+    const getDepartureStation = async() => {
+      let data = await fetch("https://www.mvg.de/api/fib/v2/location?query=Forstenrieder%20Allee", {
+        method: "GET"
+      });
+      let jsonData = await data.json();
+  
+      const pds = jsonData.find((e: DepartureStationProps) => e.type === "STATION")
+      data = await fetch(`https://www.mvg.de/api/fib/v2/departure?globalId=${pds?.globalId}&limit=10&offsetInMinutes=0`, {
+        method: "GET"
+      });
+      jsonData = await data.json();
+
+      setDepartureStation(pds)
+      setDepartures(jsonData);
+    }
+
     const getDisruptions = async () => {
       const data = await fetch("https://www.mvg.de/api/ems/tickers", {
         method: "GET"
@@ -33,22 +86,64 @@ function App() {
       const jsonData = await data.json();
       setDisruptions(jsonData);
     };
+    
+    const interval = setInterval(() => {
+      getDepartureStation();
+      getDisruptions();
+    }, 60000); // 60000 milliseconds = 1 minute
 
-    getDisruptions();
+    // Clear the interval when the component is unmounted or when the dependency array changes
+    return () => clearInterval(interval);
   }, []);
+
+  const calculateRemainingTime = (epochTime: number) => {
+    const currentTime = new Date().getTime();
+    const remainingMilliseconds = epochTime - currentTime;
+
+    // If remaining time is less than 1 minute, display "NOW"
+    if (remainingMilliseconds < 60000) {
+      return 'NOW';
+    }
+
+    // Convert remaining time to minutes and hours
+    const remainingMinutes = Math.floor(remainingMilliseconds / 60000);
+    const hours = Math.floor(remainingMinutes / 60);
+    const minutes = remainingMinutes % 60;
+
+    // Construct the remaining time string
+    let remainingTimeString = '';
+    if (hours > 0) {
+      remainingTimeString += `in ${hours} ${hours === 1 ? 'hour' : 'hours'}`;
+      if (minutes > 0) {
+        remainingTimeString += `in ${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`;
+      }
+    } else {
+      remainingTimeString += `in ${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`;
+    }
+
+    return remainingTimeString;
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        {disruptions.map((value) => {
+        <div>
+          <div>{departureStations?.name}</div>
+        </div>
+        {departures.map((departure, idx) => {
+          return (<div key={idx}>
+            <div >{departure.label} <small>to <strong>{departure.destination}</strong> {calculateRemainingTime(departure.realtimeDepartureTime)}</small></div>
+          </div>)
+        })}
+        {/* {disruptions.map((disruption, idx) => {
           return (
-            <div>
-              <div><strong>{value.type}</strong></div>
-              <div>{value.title}</div>
-              <div>{value.text}</div>
+            <div key={idx}>
+              <div><strong>{disruption.type}</strong></div>
+              <div>{disruption.title}</div>
+              <div>{disruption.text}</div>
             </div>
           );
-        })}
+        })} */}
       </header>
     </div>
   );
