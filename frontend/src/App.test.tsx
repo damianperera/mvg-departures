7ad8b824a462e42107b417728bc11bba06ecbc6c
Damian Perera <damianperera@outlook.com>
2024-02-29 17:25:34 +0100

    added snapshot test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('app skeleton renders correctly', () => {
+  const { container } = render(<BrowserRouter><App /></BrowserRouter>)
+  expect(container.firstChild).toMatchSnapshot()
+})
