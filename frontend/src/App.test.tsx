901b24505d80302e0d7f65d454ee87892d2303cd
Damian Perera <damianperera@outlook.com>
2024-03-09 18:02:13 +0100

    added eslint

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 test('app skeleton renders correctly', () => {
-  const { container } = render(<BrowserRouter><App /></BrowserRouter>)
-  expect(container.firstChild).toMatchSnapshot()
+	const { container } = render(<BrowserRouter><App /></BrowserRouter>)
+	expect(container.firstChild).toMatchSnapshot()
 })
