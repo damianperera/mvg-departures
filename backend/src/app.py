d3f5943a33b55add63ff98bf6a49425ad1e9e416
Damian Perera <damianperera@outlook.com>
2024-03-16 08:37:01 +0100

    added endpoints

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,16 @@
+import asyncio
 from flask import Flask
+from mvg import MvgApi
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/station/all')
+def stations():
+    return MvgApi.stations()
+
+@app.route('/station/<id>/departures')
+def departures(id):
+    return asyncio.run(MvgApi.departures_async(id, 20, 0, None))
 
 if __name__ == '__main__':
     app.run()
