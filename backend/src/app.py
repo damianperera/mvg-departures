efa2f4de14b53d815ee32fe73d05c8ca4ed4c8c1
Damian Perera <damianperera@outlook.com>
2024-03-16 09:26:07 +0100

    pluralised endpoints

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,11 +4,11 @@ from mvg import MvgApi
 
 app = Flask(__name__)
 
-@app.route('/station/all')
+@app.route('/stations/')
 def stations():
     return MvgApi.stations()
 
-@app.route('/station/<id>/departures')
+@app.route('/stations/<id>/departures')
 def departures(id):
     return asyncio.run(MvgApi.departures_async(id, 20, 0, None))
 
