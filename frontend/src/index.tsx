2b9695ab96c94772484d39c1560bc826b6850c4a
Damian Perera <damianperera@outlook.com>
2024-03-21 15:59:53 +0100

    refactored web vitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Metric } from 'web-vitals'
-import ReactGA from 'react-ga4'
 import reportWebVitals from './reportWebVitals'
 import App from './App'
 
-const root = ReactDOM.createRoot(
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-)
-root.render(
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
@@ -18,33 +15,4 @@ root.render(
   </React.StrictMode>
 )
 
-ReactGA.initialize([
-  {
-    trackingId: 'G-XEN77GP035'
-  }
-])
-
-const getEventValueFromMetric = (metric: Metric) => {
-  if (metric.name === 'CLS') {
-    return Math.round(metric.value * 1000)
-  }
-  return Math.round(metric.value)
-}
-
-const reportHandler = (metric: Metric) => {
-  const event = {
-    category: 'Web Vitals',
-    action: metric.name,
-    value: getEventValueFromMetric(metric),
-    label: metric.id,
-    nonInteraction: true
-  }
-
-  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
-    console.log(`[web-vitals-dev] ${metric.name}: ${metric.id}`, metric) // eslint-disable-line no-console
-  } else {
-    ReactGA.event(event)
-  }
-}
-
-reportWebVitals(reportHandler)
+reportWebVitals()
--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,15 +1,42 @@
-import { ReportHandler } from 'web-vitals'
+import { Metric } from 'web-vitals'
+import ReactGA from 'react-ga4'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    })
+const getEventValueFromMetric = (metric: Metric) => {
+  if (metric.name === 'CLS') {
+    return Math.round(metric.value * 1000)
   }
+  return Math.round(metric.value)
+}
+
+const reportHandler = (metric: Metric) => {
+  const event = {
+    category: 'Web Vitals',
+    action: metric.name,
+    value: getEventValueFromMetric(metric),
+    label: metric.id,
+    nonInteraction: true
+  }
+
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    console.debug(`[web-vitals-dev] ${event.action}: ${event.value}`, metric) // eslint-disable-line no-console
+  } else {
+    ReactGA.event(event)
+  }
+}
+
+const reportWebVitals = () => {
+  ReactGA.initialize([
+    {
+      trackingId: 'G-XEN77GP035'
+    }
+  ])
+  import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    getCLS(reportHandler)
+    getFID(reportHandler)
+    getFCP(reportHandler)
+    getLCP(reportHandler)
+    getTTFB(reportHandler)
+  })
 }
 
 export default reportWebVitals
