620e8460e1e92bf8dd777c7e5629c4e635695046
Damian Perera <damianperera@outlook.com>
2024-03-21 09:48:06 +0100

    push web vitals to google analytics

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { Metric } from 'web-vitals'
+import ReactGA from 'react-ga4'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +18,30 @@ root.render(
 	</React.StrictMode>
 )
 
+ReactGA.initialize([
+	{
+		trackingId: 'G-XEN77GP035'
+	}
+])
+
+const getEventValueFromMetric = (metric: Metric) => {
+	if (metric.name === 'CLS') {
+		return Math.round(metric.value * 1000)
+	}
+	return Math.round(metric.value)
+}
+ 
+const reportHandler = (metric: Metric) => {
+	ReactGA.event({
+		category: 'Web Vitals',
+		action: metric.name,
+		value: getEventValueFromMetric(metric),
+		label: metric.id,
+		nonInteraction: true
+	})
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals(reportHandler)
