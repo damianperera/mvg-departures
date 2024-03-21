5afeae3581fe81bf206bf7ffbf5bf83847fe2bb3
Damian Perera <damianperera@outlook.com>
2024-03-21 17:34:51 +0100

    refactored tests

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from '../App'
 
 test('app skeleton renders correctly', () => {
   const { container } = render(<BrowserRouter><App /></BrowserRouter>)
